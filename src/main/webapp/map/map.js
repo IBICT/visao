6b2fca6580667d4661e2974f9fc2ab2d83249a32
Victor Braga <victorbraga@ibict.br>
2019-04-23 17:14:17 -0300

    Ajustes de layout

--- src/main/webapp/map/map.js
+++ src/main/webapp/map/map.js
@@ -634,7 +634,9 @@ function makeMap() {
 												'</div>' +
 									  '		</div>' +
 									  '		<span onclick="menuMapControl()" style="position:relative; left:-31px; top: -730px;; color:white; background-color: #59C9A8; padding: 6px 8px;border-radius:4px 0 0 4px;"><span class="fa fa-chevron-right"> </span></span>' +
-                                      '		<div style="position: absolute;bottom: 0px;right: 0px;height: 160px;width: 100%;background-color: #F5F6F6;box-shadow: 0px -1px 5px rgba(0, 0, 0, 0.2);border-top: 2px solid #59C9A8;">' +
+                                      '		<div id="footer-sidebar-filtros">' +
+                                      ' 		<div class="indicador"></div>' +
+                                      ' 		<div class="filtro"></div>' +
 									  '		    <button id="submitFormIndicator" disabled style="margin: 10px 5px; background-color: #146678; color: #fff;" class="btn pull-right robotoFamily">Aplicar</button>' +
                                       '   		<button id="clearFormIndicator" style="margin: 10px 5px; background-color: #fff; color: #146678;" class="btn pull-right robotoFamily">Limpar</button>' +
                                       '		</div>' +
@@ -769,6 +771,8 @@ function makeMap() {
                             '<span class="robotoFamily">' +
                             dataJson[0].name.value + ' <span style="background-color: #59C9A8; padding:3.5px 6.5px; color: white; border-radius: 4px; font-size:12px; position:relative; top:-2.5px;" class="fa fa-info"></span>' +
                             '</span></div>';
+
+                        $("#footer-sidebar-filtros .indicador").html(this._div.innerHTML);
 
                         L.DomEvent.addListener(this._div, 'dblclick', L.DomEvent.stop);
                         L.DomEvent.addListener(this._div, 'mousedown', L.DomEvent.stop);
@@ -1030,6 +1034,8 @@ function loadFiltrosSelecionados(){
                 '<span class="fa fa-info"></span> Filtros selecionados' +
             '</span>'+
         '</div>';
+
+    $("#footer-sidebar-filtros .filtro").html(filtrosSelecionados);
 
     return filtrosSelecionados;
 }
