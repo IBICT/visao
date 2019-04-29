ff65ef88795ec393b44be3cf911ffa7dd97610dd
Victor Braga <victorbraga@ibict.br>
2019-04-29 14:30:55 -0300

    Correções de estilo

--- src/main/webapp/map/map.js
+++ src/main/webapp/map/map.js
@@ -638,13 +638,13 @@ function makeMap() {
 									  '		</div>' +
 									  '		<span onclick="menuMapControl()" style="position:absolute; left:-25px; top: 90px; color:white; background-color: #59C9A8; padding: 6px 8px;border-radius:4px 0 0 4px;"><span class="fa fa-chevron-right"> </span></span>' +
                                       '		<div id="footer-sidebar-filtros">' +
-                                      '		    <div class="pull-left" style="width:50%;min-height:1px;margin-top:20px">' +
+                                      '		    <div class="pull-left" style="width:60%;min-height:1px;margin-top:20px">' +
                                       ' 		    <div class="indicador" style="text-align:center"></div>' +
-                                      '     		<div class="filtro"></div>' +
+                                      '     		<div class="filtro" style="margin: 10px 45px 0 0;"></div>' +
                                       '		    </div>' +
-                                      '		    <div class="pull-left" style="width:50%;padding-right:20px">' +
-									  '    		    <button id="submitFormIndicator" disabled style="margin: 10px 5px; background-color: #146678; color: #fff;" class="btn pull-right robotoFamily">Aplicar</button><br>' +
-                                      '       		<button id="clearFormIndicator" style="margin: 10px 5px; background-color: #f5f6f6; color: #146678;" class="btn pull-right robotoFamily">Limpar&nbsp;<span class="fa fa-trash-o"></span></button>' +
+                                      '		    <div class="pull-left" style="width:40%;padding-right:20px">' +
+									  '    		    <button id="submitFormIndicator" disabled style="margin: 15px 15px 0; background-color: #146678; color: #fff;" class="btn pull-right robotoFamily">Aplicar</button><br>' +
+                                      '       		<button id="clearFormIndicator" style="margin: 0 5px; background-color: #f5f6f6; color: #146678;" class="btn pull-right robotoFamily">Limpar&nbsp;<span class="fa fa-trash-o"></span></button>' +
                                       '		    </div>' +
                                       '		</div>' +
 									  '	</div>' +
